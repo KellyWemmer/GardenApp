4cbc94a4c4f46ccc571d2940ed766fe525511cb0
Kelly <kellysue3@yahoo.com>
2025-01-08 14:07:50 -0700

    Working on DbContext

--- GardenApp/DataAccess/ApplicationDbContext.cs
+++ GardenApp/DataAccess/ApplicationDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace GardenApp.DataAccess
+﻿using GardenApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GardenApp.DataAccess
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public DbSet<PlantInfoModel> PlantInfo { get; set; }    
+        public DbSet<PlantStartModel> PlantStart { get; set; }
+        public DbSet<PlantSuccessModel> PlantSuccess { get; set; }
+        public DbSet<YearModel> Year { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
     }
 }
